b5c04278c3d4e12335008fb791ebd91f58f543b9
KubantsevAS <veng93@yandex.ru>
2023-03-08 18:41:29 +0300

    fixed sorting in LikedList

--- src/pages/HotelsPage/Main/Menus/LikedList/LikedList.jsx
+++ src/pages/HotelsPage/Main/Menus/LikedList/LikedList.jsx
@@ -1,32 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import styles from './LikedList.module.css'
 import LikedElement from './LikedElement/LikedElement'
-import { sortListByStarsUp } from '../../../../../redux/LikedListReducer'
+import { sortListByStarsUp, sortListByStarsDown } from '../../../../../redux/LikedListReducer'
+import { sortingArrowsDeactivatedSvg, sortingArrowsDownSvg, sortingArrowsUpSvg } from '../../../../../images/svgCollector'
 
 export default function Liked() {
 
-    let hotels = useSelector(store => store.reducer.LikedListReducer.hotelsData)
+    let hotels = useSelector(store => store.reducer.LikedListReducer.hotelsData);
+
+    const arrowDisable = sortingArrowsDeactivatedSvg();
+    const arrowUp = sortingArrowsUpSvg();
+    const arrowDown = sortingArrowsDownSvg();
+
+    const [starButtonState, setStarButtonState] = useState(false);
+    const [starSort, setStarSort] = useState('down')
     
     const dispatch = useDispatch();
 
-    const sortByStarsUp = () => {
-        dispatch(sortListByStarsUp())
+    const sortByStars = () => {
+        if (starSort === 'down') {
+            dispatch(sortListByStarsUp());
+            setStarButtonState(true);
+            setStarSort('up')
+        } else if (starSort === 'up') {
+            dispatch(sortListByStarsDown())
+            setStarSort('down')
+        }
     }
 
-    const sortByStarsDown = () => {
-        
-    }
+    console.log(starButtonState, starSort)
     
     const sortByPrice = () => {
 
     }
 
+
     return (
         <div className={styles.likedMenu}>
             <div className={styles.title}>Избранное</div>
             <div className={styles.sortbuttons}>
-                <button className={styles.btn} onClick={sortByStarsUp}>Рейтинг</button><button className={styles.btn}>Цена</button>
+                <button className={styles.btn} onClick={sortByStars}>
+                    <span>Рейтинг</span>
+                    <span className={styles.arrow}>{!starButtonState ? arrowDisable : starSort === 'up' ? arrowUp : arrowDown}</span>
+                </button>
+                <button className={styles.btn}>
+                    <span>Цена</span>
+                    <span className={styles.arrow}>{arrowDisable}</span>
+                </button>
             </div>
 
             <div className={styles.likedListContainer}>
--- src/redux/LikedListReducer.js
+++ src/redux/LikedListReducer.js
@@ -8,18 +8,27 @@ const initialState = {
 }
 
 const LikedListReducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
 
         case ADD_TO_LIST:
-            return {...state, 
-                hotelsData: [...state.hotelsData, action.payload]};
+            return {
+                ...state,
+                hotelsData: [...state.hotelsData, action.payload]
+            };
         case REMOVE_FROM_LIST:
-            return {...state, 
-                hotelsData: state.hotelsData.filter(item => item.hotelId !== action.payload)};
+            return {
+                ...state,
+                hotelsData: [...state.hotelsData.filter(item => item.hotelId !== action.payload)]
+            };
         case SORT_LIST_BY_STARS_UP:
-            debugger
-            return {...state,
-                hotelsData: state.hotelsData.sort((a, b) => a.stars > b.stars ? 1 : -1)
+            return {
+                ...state,
+                hotelsData: [...state.hotelsData.sort((a, b) => a.stars > b.stars ? -1 : 1)]
+            }
+        case SORT_LIST_BY_STARS_DOWN:
+            return {
+                ...state,
+                hotelsData: [...state.hotelsData.sort((a, b) => a.stars < b.stars ? -1 : 1)]
             }
         default:
             return state;
@@ -28,12 +37,12 @@ const LikedListReducer = (state = initialState, action) => {
 
 // ACTION CREATORS
 
-export const addNewHotel = (payload) => ({type: ADD_TO_LIST, payload});
+export const addNewHotel = (payload) => ({ type: ADD_TO_LIST, payload });
 
-export const removeFromLikedList = (payload) => ({type: REMOVE_FROM_LIST, payload})
+export const removeFromLikedList = (payload) => ({ type: REMOVE_FROM_LIST, payload })
 
-export const sortListByStarsUp = () => ({type: SORT_LIST_BY_STARS_UP})
+export const sortListByStarsUp = () => ({ type: SORT_LIST_BY_STARS_UP })
 
-export const sortListByStarsDown = () => ({type: SORT_LIST_BY_STARS_DOWN})
+export const sortListByStarsDown = () => ({ type: SORT_LIST_BY_STARS_DOWN })
 
 export default LikedListReducer;
