fb45324f593ee4fc7043f6a1389412351f6e3c21
KubantsevAS <veng93@yandex.ru>
2023-03-09 06:13:49 +0300

    fixing error for github pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 
+// used Hashrouter instaed of BrowserRouter only for GitHub-pages
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
