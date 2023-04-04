050107b183503825f5c9222111b623253d083725
KubantsevAS <veng93@yandex.ru>
2023-04-04 14:32:44 +0300

    consts refactoring

--- src/api/consts.js
+++ src/api/consts.js
@@ -1,8 +1,6 @@
 import moment from "moment/moment";
 
 export const MAX_HOTEL_ITEMS = 100;
-
 export const baseUrl = "https://engine.hotellook.com/api/v2/cache.json?";
-
 export const initialCheckIn = moment().format("YYYY-MM-DD");
 export const initialCheckOut = moment().add(1, "days").format("YYYY-MM-DD");
