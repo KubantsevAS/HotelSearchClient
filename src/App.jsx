51a7480fb6fb6e58bf28593d9273befe930388cc
KubantsevAS <veng93@yandex.ru>
2023-04-04 11:26:33 +0300

    app and packeges overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import AuthPage from "./pages/AuthPage";
 import HotelsPage from "./pages/HotelsPage";
 import "./App.css";
